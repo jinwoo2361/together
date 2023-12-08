58dd4ba2a809cd64bdcd62862a9a6d730bf514fc
DESKTOP-F3GRD5V\user1 <edas96@naver.com>
2023-12-08 12:37:25 +0900

    Test dlachdla 기능추가

--- src/main/java/com/sh/together/Dlachdla.java
+++ src/main/java/com/sh/together/Dlachdla.java
@@ -13,5 +13,6 @@ public class Dlachdla extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=utf-8");
         resp.getWriter().print("dlachdla👩👩👩");
+        resp.getWriter().print("🎁🎁🎁");
     }
 }
